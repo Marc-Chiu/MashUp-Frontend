d8717ee967480e4499bd81a6baf3544b4f3e7c2c
tahmidasid <47311945+tahmidasid@users.noreply.github.com>
2024-02-15 14:36:27 -0500

    changed constants folder

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 const password = 'testpassword';
 const USER_DB = 'frontierDB';
 
+/*
 export const BACKEND_URL = `mongodb+srv://mmc9967:${password}@frontier.5dsrn7a.mongodb.net/${USER_DB}?retryWrites=true&w=majority` //'http://localhost:8000';
+*/
 
+export const BACKEND_URL = process.env.REACT_APP_URL_PRE;
