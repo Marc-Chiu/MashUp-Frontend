601fa5e89c437c5f4d2a3d6df3dc207c72c6c28b
marcchiu <mmc9967@nyu.edu>
2024-04-14 15:55:45 -0400

    Each group page can now view their own restaurants

--- src/Components/GroupHome/GroupHome.jsx
+++ src/Components/GroupHome/GroupHome.jsx
@@ -1,13 +1,96 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import propTypes from 'prop-types';
+import axios from 'axios';
+
+import Navbar from '../Navbar';
+
+import { BACKEND_URL } from '../../constants';
+const RESTAURANTS_ENDPOINTS = `${BACKEND_URL}/restaurants`;
+const GROUP_ENDPOINT = `${BACKEND_URL}/groups`;
+
+function ErrorMessage({ message }) {
+  return (
+    <div className="error-message">
+      {message}
+    </div>
+  );
+}
+ErrorMessage.propTypes = {
+  message: propTypes.string.isRequired,
+};
+
+function Restaurant({ restaurant }) {
+console.log(restaurant);
+const { name, Rating, Price, Address, Cuisine } = restaurant;
+  return (
+      <div className="game-container">
+        <h2>{name}</h2>
+        <p>rating: {Rating}</p>
+        <p>price: {Price}</p>
+        <p>address: {Address}</p>
+        <p>cuisine: {Cuisine}</p>
+      </div>
+  );
+}
+
+Restaurant.propTypes = {
+  restaurant: propTypes.shape({
+    name: propTypes.string.isRequired,
+    Rating: propTypes.number.isRequired,
+    Price: propTypes.string.isRequired,
+    Address: propTypes.string.isRequired,
+    Cuisine: propTypes.string.isRequired,
+  }).isRequired,
+};
 
 function GroupPage() {
+
   const group = sessionStorage.getItem('Group');
+  const [error, setError] = useState('');
+  const [restaurants, setRestaurants] = useState([]);
+
+
+  const fetchGroupList = () =>{
+    console.log("fetching group list")
+    axios.get(GROUP_ENDPOINT + "/byname/" + sessionStorage.getItem('Group'))
+    .then((response) => {
+      console.log(response.data.Restaurants)
+      fetchRestaurants(response.data.Restaurants);
+    })
+    .catch(() => setError("error getting group data"));
+  };
+
+
+  const fetchRestaurants = (groupRest) => {
+    axios.get(RESTAURANTS_ENDPOINTS)
+      .then(({ data }) =>{
+        console.log(data.Data);
+        const keys = Object.keys(data.Data).filter(key => groupRest.includes(key));
+        const restaurants = keys.map((key) => data.Data[key]);
+        setRestaurants(restaurants);
+        console.log(restaurants);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(fetchGroupList, []);
 
   return (
     <div>
-      <h1>Group: {group}</h1>
+      <Navbar />
+      <div className="wrapper">
+
+        <header>
+          <h1>
+            All Restaurants {group}
+          </h1>
+        </header>
+        {error && <ErrorMessage message={error} />}
+        {restaurants.map((restaurant) => <Restaurant key={restaurant.name} restaurant={restaurant} />)}
+      </div>
     </div>
+
   );
 }
 
@@ -20,44 +103,6 @@ export default GroupPage;
 // import { BACKEND_URL } from '../../constants';
 // import Navbar from '../Navbar';
 
-// const RESTAURANTS_ENDPOINTS = `${BACKEND_URL}/restaurants`;
-// const GROUP_ENDPOIT = `${BACKEND_URL}/groups`;
-
-// function ErrorMessage({ message }) {
-//   return (
-//     <div className="error-message">
-//       {message}
-//     </div>
-//   );
-// }
-// ErrorMessage.propTypes = {
-//   message: propTypes.string.isRequired,
-// };
-
-// function Restaurant({ restaurant }) {
-// console.log(restaurant);
-// const { name, Rating, Price, Address, Cuisine } = restaurant;
-//   return (
-//       <div className="game-container">
-//         <h2>{name}</h2>
-//         <p>rating: {Rating}</p>
-//         <p>price: {Price}</p>
-//         <p>address: {Address}</p>
-//         <p>cuisine: {Cuisine}</p>
-//         <button>Like</button>
-//       </div>
-//   );
-// }
-
-// Restaurant.propTypes = {
-//   restaurant: propTypes.shape({
-//     name: propTypes.string.isRequired,
-//     Rating: propTypes.number.isRequired,
-//     Price: propTypes.string.isRequired,
-//     Address: propTypes.string.isRequired,
-//     Cuisine: propTypes.string.isRequired,
-//   }).isRequired,
-// };
 
 // function restaurantsObjectToArray({ Data }) {
 //   const keys = Object.keys(Data);
